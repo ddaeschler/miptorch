078cc95876dafff8eec2eebce1a77410a5cc79cd
David Daeschler <david.daeschler@gmail.com>
2026-06-21 18:55:09 -0400

    Working on rocketship example

--- examples/rocketship.py
+++ examples/rocketship.py
@@ -26,40 +26,64 @@ G = 9.8
 # -fuel[i] <= 0
 
 constraint_rhs = [starting_fuel, -starting_fuel]
-fuel_thrust_rows = [] 
+rows = [] 
 
 def zeros_row(timesteps, cols):
     return [0] * timesteps * cols
 
+def fuel_col(curr_time):
+    return curr_time
+
+def thrust_col(curr_time):
+    return timesteps + curr_time
+
 for i in range(timesteps):
     if i == 0:
+        # 2 cols per timestep: fuel and thrust
+        # laid out as f0,f1,f2,...,fn,t0,t1,t2,...,tn
         next_row = zeros_row(timesteps, 2) # 2 cols per timestep: fuel and thrust
-        next_row[0] = 1.0
-        next_row[timesteps] = 1.0
-        fuel_thrust_rows.append(next_row)
+        next_row[fuel_col(0)] = 1.0
+        next_row[thrust_col(0)] = 1.0
+        rows.append(next_row)
 
         next_row = zeros_row(timesteps, 2)
-        next_row[0] = -1.0
-        next_row[timesteps] = -1.0
-        fuel_thrust_rows.append(next_row)
+        next_row[fuel_col(0)] = -1.0
+        next_row[thrust_col(0)] = -1.0
+        rows.append(next_row)
 
     else:
         next_row = zeros_row(timesteps, 2)
-        next_row[i] = 1.0
-        next_row[i - 1] = -1.0
-        next_row[timesteps + i] = 1.0
-        fuel_thrust_rows.append(next_row)
+        next_row[fuel_col(i)] = 1.0
+        next_row[fuel_col(i - 1)] = -1.0
+        next_row[thrust_col(i)] = 1.0
+        rows.append(next_row)
+        constraint_rhs.append(0.0)
+
+        next_row = zeros_row(timesteps, 2)
+        next_row[fuel_col(i)] = -1.0
+        next_row[fuel_col(i - 1)] = 1.0
+        next_row[thrust_col(i)] = -1.0
+        rows.append(next_row)
         constraint_rhs.append(0.0)
 
         next_row = zeros_row(timesteps, 2)
         next_row[i] = -1.0
-        next_row[i - 1] = 1.0
-        next_row[timesteps + i] = -1.0
-        fuel_thrust_rows.append(next_row)
+        rows.append(next_row)
         constraint_rhs.append(0.0)
 
-        next_row = zeros_row(timesteps, 2)
-        next_row[i] = -1.0
-        fuel_thrust_rows.append(next_row)
-        constraint_rhs.append(0.0)
 
+# next we model the fuel and velocity interaction
+# our velocity is equal to our velocity at the previous timestep plus gravity now
+# minus any thrust that was applied
+# velocity[i] = velocity[i - 1] - thrust[i]
+# velocity[i] - velocity[i - 1] + thrust[i] <= 0
+
+# we'll need to append columns to the rows already started
+for row in rows:
+    row.extend([0.0] * timesteps)
+
+def veocity_column(curr_time):
+    return (2*timesteps) + curr_time
+
+for i in range(timesteps):
+    pass
